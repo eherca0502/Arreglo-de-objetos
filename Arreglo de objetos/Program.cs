93b31e28ac5ea575252471092a5e9f4af76c32bd
eherca0502 <ehc0502@outlook.com>
2023-08-26 15:51:45 -0600

    Agregar archivos de proyecto.

---
+++ Arreglo de objetos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglo_de_objetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa la cantidad de alumnos");
+            int cantidadAlumnos= int.Parse(Console.ReadLine());
+            Alumnos[] a=new Alumnos[cantidadAlumnos];
+            for (int i=0; i<cantidadAlumnos; i++)
+            {
+                Console.WriteLine("Ingresa el nombre del alumno"+(i+1));
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingresa la calificacion del alumno" + (i + 1));
+                double calificacion = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa el aula del alumno" + (i + 1));
+                char aula = Console.ReadLine()[0];
+                a[i] = new Alumnos(nombre,calificacion, aula);
+
+            }
+            double suma = 0;
+            double promedio = 0;
+            int cantidadAlumnosSalon = 0;
+            Console.WriteLine("Que aula desea promediar las calificaciones?");
+            char salon = Console.ReadLine()[0];
+            for (int i=0;i<cantidadAlumnos;i++)
+            {
+                if (salon == a[i].getAula())
+                {
+                    suma = suma + a[i].getCalificacion();
+                    cantidadAlumnosSalon++;
+                }
+            }
+            promedio = suma / cantidadAlumnosSalon;
+            Console.WriteLine("El promedio de los alumnos del salon" +salon+" es "+promedio);
+            Console.Read();
+        }
+    }
+}
